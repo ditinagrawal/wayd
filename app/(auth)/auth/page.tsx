8a0030ec1e39fd680e0b12a5ecd37e4cf7cc0cbe
ditinagrawal <ditin.agrawal05@gmail.com>
2026-02-14 13:02:55 +0530

    fix folder structure

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -1,7 +1,7 @@
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { AuthView } from "@/features/auth/views/auth";
+import { AuthView } from "@/features/auth/components/auth-view";
 import { auth } from "@/lib/auth";
 
 export default async function AuthPage() {
